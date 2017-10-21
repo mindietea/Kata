9a60539ea3543dadc93e69eb45470b4ddca30fc7
copperstick6 <copperstick6@gmail.com>
2017-10-21 12:06:09 -0500

    added initial database connector

---
+++ database/index.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+require('dotenv').load();
+
+var connection = mysql.createConnection({
+  host     : process.env.HOST,
+  user     : process.env.USER,
+  password : process.env.PASSWORD,
+  port     : process.env.PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end()

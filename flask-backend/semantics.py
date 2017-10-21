6684483ca7b738406ee570b31c420f8f3d57dddb
sanat Sharma <sanatcolumban@gmail.com>
2017-10-21 15:41:39 -0500

    arrange recommendation data

--- flask-backend/semantics.py
+++ flask-backend/semantics.py
@@ -2,20 +2,34 @@ from semantics3 import Products
 import keys
 import json
 
-k = keys.Keys()
-sem3 = Products(
-    api_key = k.get_semantics_api_key(),
-    api_secret = k.get_semantics_secret_key()
-)
+def recommendation_data(name, brand):
+    k = keys.Keys()
+    sem3 = Products(
+        api_key = k.get_semantics_api_key(),
+        api_secret = k.get_semantics_secret_key()
+    )
 
-sem3.products_field("search", "iphone")
-sem3.products_field("brand", "apple")
-sem3.products_field("limit", 7)
-results = sem3.get()
+    sem3.products_field("search", "iphone")
+    sem3.products_field("brand", "apple")
+    sem3.products_field("limit", 10)
+    results = sem3.get()
 
-for r in results["results"]:
-    print(r["images"][0])
-    print(r['sitedetails'][0]["url"])
-    print(r['sitedetails'][0]["latestoffers"][0]["price"])
-    print(r['brand'])
-    print(r['name'])
+    output = []
+
+    for r in results["results"]:
+        data = {}
+        data['image'] = r["images"][0]
+        data['url'] = r['sitedetails'][0]["url"]
+    
+        try:
+            data['price'] = r['sitedetails'][0]["latestoffers"][0]["price"]
+        except:
+            pass
+        
+        data['brand'] = r['brand']
+        data['name'] = r['name']
+        output.append(data)
+        print(data)
+    return output
+
+recommendation_data("macbook", "apple")

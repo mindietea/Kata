b7661067d6d00bcca4e1e9e2c015ca6284498dd5
David Chen <davidzchen@utexas.edu>
2017-10-21 15:15:45 -0500

    finished semantics and database flask endpoints

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -8,27 +8,31 @@ def hello():
 
 @app.route("/api/post/<post_id>")
 def get_post(post_id):
-    payload = {
-       "post_id": post_id
-    }
-    response = requests.get(url, params=payload)
-    return jsonify(response.json())
+    return database.get_post(post_id)
+
+@app.route("/api/post/recommendations")
+def get_recommendations():
+    product_name = requests.args.name,
+    product_vendor = requests.args.vendor
+    return semantics.get_recommendations(product_name, product_vendor)
 
 @app.route("/api/curator/<curator_id>/posts")
 def get_curator_posts(curator_id):
-    payload = {
-        "curator_id": curator_id
-    }
-    response = requests.get(url, params=payload)
-    return jsonify(response.json())
+    return database.get_curator_posts(curator_id)
 
 @app.route("/api/post/create", methods=['POST'])
 def create_post():
     body = request.get_json()
-    images = body["images"]
+
+    curator = body["curator"]
+    date = body["date"]
     title = body["title"]
     desc = body["description"]
-    
-    # insert sql connector here
+    in_stock = body["in_stock"]
+    sizes = body["sizes"]
+    product_link = body["product_link"]
+    product_name = body["product_name"]
+    image_link = body["image"]
+    image_name = body["image_name"]
 
-    return jsonify(response.json())
+    return database.create_post(curator, date, title, desc, in_stock, sizes, product_link, product_name, image_link, image_name)
